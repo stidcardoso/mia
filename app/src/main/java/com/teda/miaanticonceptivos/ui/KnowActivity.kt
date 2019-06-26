cf7af9274d347efddc7b0fd83d4a3b0aaba3de50
Stid Cardoso <animestid@hotmail.com>
2019-06-26 01:48:34 -0500

    .

--- app/src/main/java/com/teda/miaanticonceptivos/ui/KnowActivity.kt
+++ app/src/main/java/com/teda/miaanticonceptivos/ui/KnowActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.teda.miaanticonceptivos.R
+import kotlinx.android.synthetic.main.activity_know.*
 
 
 class KnowActivity : AppCompatActivity() {
@@ -12,6 +14,7 @@ class KnowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_know)
+        Glide.with(this).asGif().load(R.raw.loading).into(imageView)
         Handler().postDelayed({
             /* Create an Intent that will start the Menu-Activity. */
             val mainIntent = Intent(this, MainActivity::class.java)
--- app/src/main/java/com/teda/miaanticonceptivos/ui/form/view/SplashActivity.kt
+++ app/src/main/java/com/teda/miaanticonceptivos/ui/form/view/SplashActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.teda.miaanticonceptivos.R
 import com.teda.miaanticonceptivos.ui.form.presenter.SplashContract
 import com.teda.miaanticonceptivos.ui.form.presenter.SplashPresenter
+import kotlinx.android.synthetic.main.activity_splash.*
+
+
 
 
 class SplashActivity : AppCompatActivity(), SplashContract.View {
@@ -17,6 +21,7 @@ class SplashActivity : AppCompatActivity(), SplashContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         presenter.getFirebaseData()
+        Glide.with(this).asGif().load(R.raw.loading).into(imageView)
     }
 
     override fun goToNextActivity() {
