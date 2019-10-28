2a6e35374e631aee7557315d41ddd6ce23d6b3c7
Stid Cardoso <animestid@hotmail.com>
2019-10-27 22:06:33 -0500

    Update faces and discreet in "Implante"

--- app/src/main/java/com/teda/miaanticonceptivos/util/Storage.kt
+++ app/src/main/java/com/teda/miaanticonceptivos/util/Storage.kt
@@ -22,7 +22,7 @@ object Storage {
             Method(2, "Vasectomia", 1000, 2, R.drawable.vasectomia_res),
             Method(3, "T de cobre", 36, 2, R.drawable.cobre_res),
             Method(4, "T de hormonas", 36, 2, R.drawable.hormonas_res),
-            Method(5, "Implante", 36, 1, R.drawable.implante),
+            Method(5, "Implante", 36, 2, R.drawable.implante),
             Method(6, "Inyectables", 3, 1, R.drawable.inyectables_res),
             Method(7, "Orales", 0, 1, R.drawable.orales_res),
             Method(8, "Parche", 1, 1, R.drawable.parche_res),
@@ -32,15 +32,14 @@ object Storage {
     var compareList = arrayListOf(
             arrayListOf(3, 3, 3, 3, 3, 2, 2, 2, 2, 1),
             arrayListOf(3, 3, 2, 2, 2, 2, 2, 2, 2, 3),
-            arrayListOf(3, 0, 2, 3, 2, 1, 1, 1, 2, 3),
-            arrayListOf(0, 0, 2, 0, 3, 2, 2, 2, 2, 0),
-            arrayListOf(0, 0, 2, 0, 3, 2, 2, 2, 3, 0),
-            arrayListOf(3, 0, 3, 3, 3, 2, 2, 2, 3, 0),
+            arrayListOf(3, 3, 3, 2, 0, 1, 1, 1, 0, 3),
+            arrayListOf(0, 0, 0, 3, 3, 2, 2, 2, 2, 0),
+            arrayListOf(0, 0, 0, 2, 2, 2, 2, 2, 2, 0),
+            arrayListOf(3, 0, 3, 2, 2, 2, 2, 2, 2, 0),
             arrayListOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 3),
+            arrayListOf(3, 3, 3, 2, 2, 1, 1, 1, 1, 1),
             arrayListOf(3, 3, 3, 3, 3, 1, 1, 1, 1, 1),
-            arrayListOf(3, 3, 3, 3, 3, 1, 1, 1, 1, 1),
-            arrayListOf(3, 3, 2, 3, 3, 2, 1, 1, 3, 1)
-
+            arrayListOf(3, 3, 3, 3, 2, 2, 1, 1, 2, 1)
     )
 
     var imagesNames = arrayListOf(
